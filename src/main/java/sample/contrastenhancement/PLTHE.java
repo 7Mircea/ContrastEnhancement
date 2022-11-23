ff169b2a8e85b4caa1f97fa22c8517e7f62f119a
Gheoace Mircea <7mircea.c@gmail.com>
2022-11-23 15:45:17 +0200

    change datatype for allmost all variables except arr that before have type byte. Too much logical errors because of byte usage

--- src/main/java/sample/contrastenhancement/PLTHE.java
+++ src/main/java/sample/contrastenhancement/PLTHE.java
@@ -4,23 +4,25 @@ import com.sun.istack.internal.NotNull;
 
 import java.util.SortedMap;
 
-import static sample.utils.ChangeType.btoS;
+import static sample.utils.ChangeType.*;
 
 public class PLTHE {
     public static void he(int height, int width, @NotNull Histogram histogramObj) {
-        byte[] partitionThresholdPoints = new byte[]{(byte) -128, (byte) 127};
-        long[] arrMedians = histogramObj.getMediansInPartitions(partitionThresholdPoints);
-        assert arrMedians != null && arrMedians.length == 1;
-        long median = arrMedians[0];
-        long[] arrMeans = histogramObj.getMeansInHistogram(partitionThresholdPoints);
-        assert arrMeans != null && arrMeans.length == 1;
-        long mean = arrMeans[0];
-        long clippingLimit = (median + mean) / 2;
+        short[] partitionThresholdPoints = new short[]{0, 255};
+        int[] arrHistogramMedians = histogramObj.getMediansInPartitions(partitionThresholdPoints);
+        assert arrHistogramMedians != null && arrHistogramMedians.length == 1;
+        int histogramMedian = arrHistogramMedians[0];
+        int[] arrHistogramMeans = histogramObj.getMeansInHistogram(partitionThresholdPoints);
+        assert arrHistogramMeans != null && arrHistogramMeans.length == 1;
+        int histogramMean = arrHistogramMeans[0];
+        int clippingLimit = (histogramMedian + histogramMean) / 2;
         System.out.println("clipping limit : " + clippingLimit);
-        histogramObj.clippTheHistogram(partitionThresholdPoints, new long[]{clippingLimit});
-        partitionThresholdPoints = getGrayLevels(histogramObj, mean);
+        histogramObj.clippTheHistogram(partitionThresholdPoints, new int[]{clippingLimit});
+        short[] grayLevelMeans = histogramObj.getMeansGrayLevelInHistogram(new short[]{0, 255});
+        short grayLevelMean = grayLevelMeans[0];
+        partitionThresholdPoints = getGrayLevels(histogramObj, grayLevelMean);
         System.out.println("partition points ");
-        for (byte el:partitionThresholdPoints) {
+        for (short el : partitionThresholdPoints) {
             System.out.print(el + " ");
         }
 
@@ -30,22 +32,22 @@ public class PLTHE {
         assert arr != null && arr.length > 0;
         assert histogramObj.getCdf().size() == 3;
         assert histogramObj.getPdf().size() == 3;
-        SortedMap<Byte, Double> cdf1 = histogramObj.getCdf().get(0);
-        SortedMap<Byte, Double> cdf2 = histogramObj.getCdf().get(1);
-        SortedMap<Byte, Double> cdf3 = histogramObj.getCdf().get(2);
-        SortedMap<Byte, Double> pdf1 = histogramObj.getPdf().get(0);
-        SortedMap<Byte, Double> pdf2 = histogramObj.getPdf().get(1);
-        SortedMap<Byte, Double> pdf3 = histogramObj.getPdf().get(2);
+        SortedMap<Short, Double> cdf1 = histogramObj.getCdf().get(0);
+        SortedMap<Short, Double> cdf2 = histogramObj.getCdf().get(1);
+        SortedMap<Short, Double> cdf3 = histogramObj.getCdf().get(2);
+        SortedMap<Short, Double> pdf1 = histogramObj.getPdf().get(0);
+        SortedMap<Short, Double> pdf2 = histogramObj.getPdf().get(1);
+        SortedMap<Short, Double> pdf3 = histogramObj.getPdf().get(2);
 
         for (int i = 0; i < height; ++i) {
             for (int j = 0; j < width; ++j) {
-                byte grayLevel = arr[j + i * width];
+                short grayLevel = btoS(arr[j + i * width]);
                 if (grayLevel >= partitionThresholdPoints[0] && grayLevel < partitionThresholdPoints[1]) {
-                    arr[j + i * width] = (byte) (btoS(partitionThresholdPoints[1]) * (cdf1.get(grayLevel) - 0.5D * pdf1.get(grayLevel)) - 128);
+                    arr[j + i * width] = dtoB(partitionThresholdPoints[1] * cdf1.get(grayLevel));
                 } else if (grayLevel >= partitionThresholdPoints[1] && grayLevel < partitionThresholdPoints[2]) {
-                    arr[j + i * width] = (byte) (btoS(partitionThresholdPoints[1]) + (partitionThresholdPoints[2] - partitionThresholdPoints[1]) * (cdf2.get(grayLevel) - 0.5D * pdf2.get(grayLevel)) - 128);
+                    arr[j + i * width] = dtoB(partitionThresholdPoints[1] + (partitionThresholdPoints[2] - partitionThresholdPoints[1]) * cdf2.get(grayLevel));
                 } else if (grayLevel >= partitionThresholdPoints[2] && grayLevel <= partitionThresholdPoints[3]) {
-                    arr[j + i * width] = (byte) (btoS(partitionThresholdPoints[2]) + (partitionThresholdPoints[3] - partitionThresholdPoints[2]) * (cdf3.get(grayLevel) - 0.5D * pdf3.get(grayLevel)) - 128);
+                    arr[j + i * width] = dtoB(partitionThresholdPoints[2] + (partitionThresholdPoints[3] - partitionThresholdPoints[2]) * cdf3.get(grayLevel));
                 }
             }
         }
@@ -57,10 +59,10 @@ public class PLTHE {
      * @param histogram
      * @return
      */
-    private static byte[] getGrayLevels(final Histogram histogram, long mean) {
-        byte[] partitionThresholdPoints = new byte[4];
+    private static short[] getGrayLevels(final Histogram histogram, int mean) {
+        short[] partitionThresholdPoints = new short[4];
         partitionThresholdPoints[0] = -128;
-        byte[] partitionInnerThresholdPoints = getInnerGrayLevels(histogram, mean);
+        short[] partitionInnerThresholdPoints = getInnerGrayLevels(histogram, mean);
         assert partitionInnerThresholdPoints.length == 2;
         partitionThresholdPoints[1] = partitionInnerThresholdPoints[0];
         partitionThresholdPoints[2] = partitionInnerThresholdPoints[1];
@@ -68,14 +70,14 @@ public class PLTHE {
         return partitionThresholdPoints;
     }
 
-    private static byte[] getInnerGrayLevels(final Histogram hist, long mean) {
-        long nrOfPixels = hist.calculateNumberOfPixels((byte) -1);
-        long sum = 0;
-        for (SortedMap.Entry<Byte, Long> element : hist.getHist().entrySet()) {
+    private static short[] getInnerGrayLevels(final Histogram hist, long mean) {
+        int nrOfPixels = hist.calculateNumberOfPixels((byte) -1);
+        double sum = 0;
+        for (SortedMap.Entry<Short, Integer> element : hist.getHist().entrySet()) {
             sum += (element.getKey() - mean) * (element.getKey() - mean) * element.getValue();
         }
-        byte std_dev = (byte) (sum / nrOfPixels);
+        short std_dev = (short) (sum / nrOfPixels);
 
-        return new byte[]{(byte) (((byte) -128) + std_dev), (byte) (((byte) 127) - std_dev)};
+        return new short[]{std_dev, (short) (255 - std_dev)};
     }
 }
